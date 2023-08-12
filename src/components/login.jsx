de2c1d28e2a782b37fa3a2f3f354d7f0168cf784
Sohini Bhattacharjee <sohinismail@gmail.com>
2023-08-12 16:05:24 +0530

    Improved UI of Login Page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import backgroundImage from "../Assets/Login page image.png";
+import backgroundImage from "../Assets/Inventory_image.jpg";
+import logo from "../Assets/flat-color-icons_google.png";
 import { Box, TextField, Button, Typography } from "@mui/material";
 import { styled } from "@mui/system";
 
@@ -10,23 +11,49 @@ const Component = styled(Box)`
 `;
 
 const FormComponent = styled(Box)`
-  width: 450px;
+  width: 550px;
+  padding: 50px 45px;
+  border: 1px solid gray;
+  border-radius: 15px;
   display: flex;
   flex-direction: column;
-  gap: 50px;
-  margin-top: 50px;
-`;
-
-const Heading = styled(Typography)`
-  font-size: 24px;
+  gap: 40px;
+  margin: 50px 35px;
 `;
 
 const Tagline = styled(Typography)`
-  font-size: 20px;
+  font-size: 30px;
+  color: rgba(0, 0, 0, 68%);
 `;
 
+const SignInButton = styled(Button)`
+  font-size: 15px;
+  text-transform: none;
+  color: #000000;
+  border: 1px solid #000000;
+  border-radius: 30px;
+  height: 52px;
+`;
+
+const LoginButton = styled(Button)`
+  font-size: 15px;
+  background-color: #101540;
+  border-radius: 30px;
+  color: #ffffff;
+  text-transform: none;
+  height: 60px;
+`;
+
+const Input = styled(TextField)`
+  background-color: #eef5f9;
+`;
+
+const Logo = styled("img")({
+  marginLeft: "5px",
+});
 const ImageComponent = styled("img")({
-  width: "830px",
+  marginTop: "30px",
+  width: "700px",
   height: "700px",
 });
 
@@ -34,20 +61,18 @@ function login() {
   return (
     <Component>
       <FormComponent>
-        <Heading variant="h1">Login</Heading>
         <Tagline variant="h6">See Your Growth and Get Support!</Tagline>
 
-        <Button type="submit" variant="outlined">
-          Sign in with Google
-        </Button>
-
-        <TextField type="email" label="Email" variant="outlined" />
-        <TextField type="password" label="Password" variant="outlined" />
-        <Button type="submit" variant="contained">
+        <Input type="email" label="Email" variant="outlined" />
+        <Input type="password" label="Password" variant="outlined" />
+        <LoginButton type="submit" variant="contained">
           Login
-        </Button>
+        </LoginButton>
 
         <Typography>Not registered yet? Create a new account</Typography>
+        <SignInButton type="submit" variant="outlined">
+          Sign in with Google <Logo src={logo} alt="" />
+        </SignInButton>
       </FormComponent>
 
       <ImageComponent
